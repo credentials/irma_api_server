529ee7e8acd8906fc0f93cfca28d12cec0cfc2b8
Sietse Ringers <mail@sietseringers.net>
2016-01-18 11:49:34 +0100

    Better session handling, include DELETE listener

--- src/main/java/org/irmacard/api/web/IssueResource.java
+++ src/main/java/org/irmacard/api/web/IssueResource.java
@@ -30,16 +30,20 @@ public class IssueResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public ClientQr create(IdentityProviderRequest isRequest) throws InfoException {
+	public ClientQr create(IdentityProviderRequest isRequest) {
 		IssuingRequest request = isRequest.getRequest();
 
 		if (request == null || request.getCredentials() == null || request.getCredentials().size() == 0)
 			throw new InputInvalidException("Incomplete request");
 
-		// Check if we have all necessary secret keys
-		for (CredentialRequest cred : request.getCredentials()) {
-			IssuerDescription id = DescriptionStore.getInstance().getIssuerDescription(cred.getIssuerName());
-			IdemixKeyStore.getInstance().getSecretKey(id); // Throws InfoException if we don't have it, TODO handle better
+		try {
+			// Check if we have all necessary secret keys
+			for (CredentialRequest cred : request.getCredentials()) {
+				IssuerDescription id = DescriptionStore.getInstance().getIssuerDescription(cred.getIssuerName());
+				IdemixKeyStore.getInstance().getSecretKey(id); // Throws InfoException if we don't have it
+			}
+		} catch (InfoException e) {
+			throw new WebApplicationException("Missing Idemix secret key", Response.Status.UNAUTHORIZED);
 		}
 
 		request.setNonceAndContext();
@@ -60,7 +64,7 @@ public class IssueResource {
 	public IssuingRequest get(@PathParam("sessiontoken") String sessiontoken) {
 		IssueSession session = sessions.getNonNullSession(sessiontoken);
 		if (session.getStatus() != IssueSession.Status.INITIALIZED) {
-			throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+			fail(new WebApplicationException(Response.Status.UNAUTHORIZED), session);
 		}
 
 		System.out.println("Received get, token: " + sessiontoken);
@@ -74,10 +78,10 @@ public class IssueResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<IssueSignatureMessage> getSignatureMessages(IssueCommitmentMessage commitments,
-			@PathParam("sessiontoken") String sessiontoken) throws InfoException, CredentialsException {
+			@PathParam("sessiontoken") String sessiontoken) throws WebApplicationException {
 		IssueSession session = sessions.getNonNullSession(sessiontoken);
 		if (session.getStatus() != IssueSession.Status.CONNECTED) {
-			throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+			fail(new WebApplicationException(Response.Status.UNAUTHORIZED), session);
 		}
 
 		System.out.println("Received commitments, token: " + sessiontoken);
@@ -86,42 +90,79 @@ public class IssueResource {
 		ProofList proofs = commitments.getCombinedProofs();
 		int credcount = request.getCredentials().size();
 		if (proofs.size() < credcount)
-			throw new InputInvalidException("Proof count does not match credential count");
+			fail(new InputInvalidException("Proof count does not match credential count"), session);
 
 		// Lookup the public keys of any ProofD's in the proof list
 		proofs.populatePublicKeyArray();
 
-		// Lookup the public keys of all ProofU's in the proof list. We have to do this before we can compute the CL
-		// sigatures below, because that also verifies the proofs, which needs these keys.
-		proofs.populatePublicKeyArray();
-		int disclosureCount = proofs.getProofDCount();
-		for (int i = 0; i < credcount; i++) {
-			CredentialRequest cred = request.getCredentials().get(i);
-			proofs.setPublicKey(disclosureCount + i, cred.getPublicKey());
-		}
-
-		// If any disclosures are required before we give the credentials, verify that they are present and correct
-		if (request.getRequiredAttributes().size() > 0) {
-			DisclosureProofRequest disclosureRequest = new DisclosureProofRequest(
-					request.getNonce(), request.getContext(), request.getRequiredAttributes());
-			if (disclosureRequest.verify(proofs).getStatus() != DisclosureProofResult.Status.VALID) {
-				throw new InputInvalidException("Incorrect disclosure proof");
+		try {
+			// Lookup the public keys of all ProofU's in the proof list. We have to do this before we can compute the CL
+			// sigatures below, because that also verifies the proofs, which needs these keys.
+			proofs.populatePublicKeyArray();
+			int disclosureCount = proofs.getProofDCount();
+			for (int i = 0; i < credcount; i++) {
+				CredentialRequest cred = request.getCredentials().get(i);
+				proofs.setPublicKey(disclosureCount + i, cred.getPublicKey());
 			}
+
+			// If any disclosures are required before we give the credentials, verify that they are present and correct
+			if (request.getRequiredAttributes().size() > 0) {
+				DisclosureProofRequest disclosureRequest = new DisclosureProofRequest(
+						request.getNonce(), request.getContext(), request.getRequiredAttributes());
+				if (disclosureRequest.verify(proofs).getStatus() != DisclosureProofResult.Status.VALID) {
+					fail(new InputInvalidException("Incorrect disclosure proof"), session);
+				}
+			}
+
+			// Construct the CL signature for each credential to be issued.
+			// FIXME This also checks the validity of _all_ proofs, for each iteration - so more than once
+			ArrayList<IssueSignatureMessage> sigs = new ArrayList<>(credcount);
+			for (int i = 0; i < credcount; i++) {
+				CredentialRequest cred = request.getCredentials().get(i);
+				IdemixSecretKey sk = IdemixKeyStore.getInstance().getSecretKey(cred.getIssuerDescription());
+
+				IdemixIssuer issuer = new IdemixIssuer(cred.getPublicKey(), sk, request.getContext());
+				sigs.add(issuer.issueSignature(
+						commitments, cred.convertToBigIntegers(), i, request.getNonce()));
+			}
+
+			session.setStatusDone();
+			return sigs;
+		} catch (InfoException|CredentialsException e) {
+			fail(new WebApplicationException(e), session);
+			return null;
 		}
+	}
 
-		// Construct the CL signature for each credential to be issued.
-		// FIXME This also checks the validity of _all_ proofs, for each iteration - so more than once
-		ArrayList<IssueSignatureMessage> sigs = new ArrayList<>(credcount);
-		for (int i = 0; i < credcount; i++) {
-			CredentialRequest cred = request.getCredentials().get(i);
-			IdemixSecretKey sk = IdemixKeyStore.getInstance().getSecretKey(cred.getIssuerDescription());
+	@DELETE
+	@Path("/{sessiontoken}")
+	public void delete(@PathParam("sessiontoken") String sessiontoken) {
+		IssueSession session = sessions.getNonNullSession(sessiontoken);
+		System.out.println("Received delete, token: " + sessiontoken);
 
-			IdemixIssuer issuer = new IdemixIssuer(cred.getPublicKey(), sk, request.getContext());
-			sigs.add(issuer.issueSignature(
-					commitments, cred.convertToBigIntegers(), i, request.getNonce()));
-		}
+		session.setStatusCancelled();
+	}
 
-		session.setStatusDone();
-		return sigs;
+	@GET
+	@Path("/{sessiontoken}/getstatus")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getStatus(@PathParam("sessiontoken") String sessiontoken) {
+		IssueSession session = sessions.getNonNullSession(sessiontoken);
+		System.out.println("Received status query, token: " + sessiontoken);
+
+		IssueSession.Status status = session.getStatus();
+		if (status == IssueSession.Status.DONE || status == IssueSession.Status.CANCELLED)
+			sessions.remove(session);
+
+		return status.toString();
+	}
+
+	/**
+	 * Removes the session, informs the identity provider, and throws the exception to notify the token.
+	 * @throws WebApplicationException The specified exception
+	 */
+	private void fail(WebApplicationException e, IssueSession session) throws WebApplicationException {
+		session.setStatusCancelled();
+		throw e;
 	}
 }
