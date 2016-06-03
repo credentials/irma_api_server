1771ee04b3f1797e0121abe59a64f32a9d19817d
Sietse Ringers <mail@sietseringers.net>
2016-06-03 16:03:23 +0200

    Put test config and keys in test resources

--- preparetest.sh
+++ preparetest.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-path=./src/main/resources
+main=./src/main/resources
+test=./src/test/resources
+
+# Symlink irma_configuration if it is present in ../
+if [ ! -d $main/irma_configuration ] && [ -d ../irma_configuration ]; then
+    ln -s ../../../../irma_configuration $main/
+fi
 
 # Generate JWT signing keys
-./keygen.sh $path/sk $path/pk
-rm $path/sk.pem 2> /dev/null # Not needed
+./keygen.sh $main/sk $main/pk
+rm $main/sk.pem 2> /dev/null # Not needed
 
 # Check if JWT private key for unit tests exists; if it does, then
 # we might as well assume that the corresponding public keys also exist
-if [ ! -e $path/test-sk.der ]; then
+if [ ! -e $test/test-sk.der ]; then
     # Generate JWT keys for unit tests
-    ./keygen.sh $path/test-sk $path/test-pk
+    ./keygen.sh $test/test-sk $test/test-pk
 
     # Move them to the appropriate locations
-    mkdir $path/verifiers $path/issuers
-    cp $path/test-pk.der $path/verifiers/testsp.der
-    mv $path/test-pk.der $path/issuers/testip.der
+    mkdir $test/verifiers $test/issuers
+    cp $test/test-pk.der $test/verifiers/testsp.der
+    mv $test/test-pk.der $test/issuers/testip.der
 
     # PEM versions are not needed
-    rm $path/test-sk.pem $path/test-pk.pem
-fi
-
-# Symlink irma_configuration if it is present in ../
-if [ ! -d $path/irma_configuration ] && [ -d ../irma_configuration ]; then
-    ln -s ../../../../irma_configuration $path/irma_configuration
+    rm $test/test-sk.pem $test/test-pk.pem
 fi
